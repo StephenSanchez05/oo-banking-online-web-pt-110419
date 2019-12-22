29fb0b9bb05750d9af7e3d02ae525bf660e5addb
Stephen Sanchez <stephensanchez05@gmail.com>
2019-12-22 19:13:31 +0000

    Automatically backed up by Learn

--- lib/transfer.rb
+++ lib/transfer.rb
@@ -1,3 +1,5 @@
+require 'pry'
+
 class Transfer
   
   attr_accessor :sender, :receiver, :status, :amount
@@ -10,7 +12,7 @@ class Transfer
   end
   
   def valid?(name)
-    name.valid?
+   binding.pry name.valid?
   end
   
 end
