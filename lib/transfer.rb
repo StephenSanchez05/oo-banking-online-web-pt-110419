0c3496301e5b0a0532ebbfdb52553c46da8de63b
Stephen Sanchez <stephensanchez05@gmail.com>
2019-12-22 19:05:37 +0000

    Automatically backed up by Learn

--- lib/transfer.rb
+++ lib/transfer.rb
@@ -2,6 +2,14 @@ class Transfer
   
   attr_accessor :sender, :receiver, :status, :amount
   
-  def initialize()
+  def initialize(sender, receiver, amount)
+    @sender = sender
+    @receiver = receiver
+    @amount = amount
+    @status = "pending"
+  end
+  
+  def valid?(name)
+    
   
 end
