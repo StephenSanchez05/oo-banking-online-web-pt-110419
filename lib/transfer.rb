ba6117aaac973096334033ff65867ea581b45fb4
Stephen Sanchez <stephensanchez05@gmail.com>
2019-12-22 19:32:05 +0000

    Automatically backed up by Learn

--- lib/transfer.rb
+++ lib/transfer.rb
@@ -25,10 +25,10 @@ class Transfer
   end
   
   describe reverse_transfer
-  if status == "complete"
+  if @status == "complete"
     sender.deposit(amount)
     receiver.deposit(-amount)
-    status = "pending"
+    @status = "pending"
   end
 
 end
