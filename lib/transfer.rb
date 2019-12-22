713db5c53e137840696af6d2956b1592feb6a990
Stephen Sanchez <stephensanchez05@gmail.com>
2019-12-22 19:29:31 +0000

    Automatically backed up by Learn

--- lib/transfer.rb
+++ lib/transfer.rb
@@ -14,7 +14,7 @@ class Transfer
   end
   
   def execute_transaction
-    if @status == "pending" && valid? && sender
+    if @status == "pending" && valid? && sender.balance > amount
       sender.deposit(-amount)
       receiver.deposit(amount)
       @status = "complete"
